695959bc0722de61362171a5210087083de46fe2
okan.toper <okan.toper94@gmail.com>
2022-03-15 10:34:14 +0300

    data succesfully fetched

---
+++ pages/components/characters.js
@@ -0,0 +1,10 @@
+export default function Characters({characters}){
+    console.log(characters)
+    return(
+        <div>
+            {/* {characters.results.map(character => {
+                <pre>{character | JSON}</pre>
+            })} */}
+        </div>
+    )
+}
